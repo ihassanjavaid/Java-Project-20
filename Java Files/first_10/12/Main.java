c4650c9c693c2341186e22a55e9bcee49afcad97
ihassanjavaid <great.maliks@gmail.com>
2020-04-30 23:33:49 +0500

    Create Main.java

---
+++ Java Files/first_10/12/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        // Make Arrays
+        int[] array1 = { 1, 2, 3, 6, 7, 8 };
+        int[] array2 = { 1, 3, 3, 6, 8, 8, 7 };
+        int[] array3 = { 7, 2, 9, 1, 2, 3, 6, 8, 9 };
+
+        // Print true/false according to pattern
+        System.out.println(arrayOneTwoThree(array1));
+        System.out.println(arrayOneTwoThree(array2));
+        System.out.println(arrayOneTwoThree(array3));
+
+    }
+
+    public static boolean arrayOneTwoThree(int[] array) {
+
+        try{
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == 1) {
+                    if (array[i + 1] == 2) {
+                        if (array[i + 2] == 3) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e){
+            return false;
+        }
+        return false;
+    }
+}
